050baf72c09d28ee8e9015dca2bdde2bda5231c9
EMachad0 <elitonmachadod200@gmail.com>
2023-07-26 17:56:01 -0300

    note created_by

--- api/src/graphql/types/note.rs
+++ api/src/graphql/types/note.rs
@@ -1,7 +1,14 @@
 use entity::note;
+use entity::prelude::User;
+use sea_orm::EntityTrait;
 use uuid::Uuid;
 
+use crate::context::Context;
+use crate::graphql::errors::GqlError;
+use crate::graphql::types::user::UserType;
+
 #[derive(Debug, SimpleObject)]
+#[graphql(complex)]
 pub struct NoteType {
     #[graphql(skip)]
     pub id: i32,
@@ -10,6 +17,23 @@ pub struct NoteType {
     pub title: String,
     pub color: String,
     pub content: String,
+    pub created_by_id: i32,
+}
+
+#[ComplexObject]
+impl NoteType {
+    pub async fn created_by(
+        &self,
+        ctx: &async_graphql::Context<'_>,
+    ) -> async_graphql::Result<UserType> {
+        let app_ctx = Context::from_context(ctx);
+        let conn = app_ctx.database.get_connection();
+        let user = User::find_by_id(self.created_by_id)
+            .one(conn)
+            .await?
+            .ok_or(GqlError::NotFound)?;
+        Ok(user.into())
+    }
 }
 
 impl From<note::Model> for NoteType {
@@ -20,6 +44,7 @@ impl From<note::Model> for NoteType {
             title: value.title,
             color: value.color,
             content: value.content,
+            created_by_id: value.created_by,
         }
     }
 }
